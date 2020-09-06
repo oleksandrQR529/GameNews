c32357025e1546461c909e155954c3c5a6ead68b
Oleksandr <sashadra21@gmail.com>
2020-09-06 18:50:41 +0300

    2

--- Game News/Controller/NewsVC.swift
+++ Game News/Controller/NewsVC.swift
@@ -11,24 +11,31 @@ import UIKit
 class NewsVC: UIViewController {
     
     @IBOutlet weak var topNewsCollection: UICollectionView!
-
+    @IBOutlet weak var newsTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initUI()
+    }
+    
+    func initUI() {
         topNewsCollection.dataSource = self
         topNewsCollection.delegate = self
+        
+        newsTable.dataSource = self
+        newsTable.delegate = self
     }
-
 }
 
-extension NewsVC: UICollectionViewDataSource, UICollectionViewDelegate {
+extension NewsVC: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3 //top news count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "topNewsCell", for: indexPath) as? TopNewsCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "topNewsCollectionCell", for: indexPath) as? TopNewsCell {
             cell.updateCell(img: UIImage(named: "digital.png")!, title: "Mersedes-Benz and Laureus broadening their worldwide involvement", source: "Apple.com", publicationDate: "- 2 hours ago")
             return cell
         }else {
@@ -36,5 +43,23 @@ extension NewsVC: UICollectionViewDataSource, UICollectionViewDelegate {
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: self.view.frame.height)
+    }
+}
+
+extension NewsVC: UITableViewDataSource, UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "newsTableCell", for: indexPath) as? newsTableCell {
+            cell.updateCell(img: UIImage(named: "digital.png")!, title: "Mersedes-Benz and Laureus broadening their worldwide involvement", source: "Apple.com", publicationDate: "- 2 hours ago")
+            return cell
+        }else {
+            return newsTableCell()
+        }
+    }
 }
---
+++ Game News/View/newsTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  newsTableCell.swift
+//  Game News
+//
+//  Created by Саша on 06.09.2020.
+//  Copyright © 2020 Саша. All rights reserved.
+//
+
+import UIKit
+
+class newsTableCell: UITableViewCell {
+    
+    @IBOutlet weak var newsImg: UIImageView!
+    @IBOutlet weak var newsTitle: UILabel!
+    @IBOutlet weak var newsSource: UILabel!
+    @IBOutlet weak var newsPublicationDate: UILabel!
+    
+    func updateCell(img: UIImage, title: String, source: String, publicationDate: String) {
+        newsImg.image = img
+        newsTitle.text = title
+        newsSource.text = source
+        newsPublicationDate.text = publicationDate
+    }
+}
