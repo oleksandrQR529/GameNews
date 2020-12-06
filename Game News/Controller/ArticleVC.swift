b2a6d36ceda228e64d4cecdcf354f4cfd1b9357b
Oleksandr <sashadra21@gmail.com>
2020-12-06 18:01:30 +0200

    1

---
+++ Game News/Controller/ArticleVC.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleVC.swift
+//  Game News
+//
+//  Created by Саша Дранчук on 06.12.2020.
+//  Copyright © 2020 Саша. All rights reserved.
+//
+
+import UIKit
+
+class ArticleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initUI()
+    }
+    
+    private func initUI() {
+        //Init UI here
+    }
+
+}
